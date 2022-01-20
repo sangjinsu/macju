689b7ed2f0c6e4d80340ad2af51d92765504f61c
ssong_14 <ssong_14@naver.com>
2022-01-20 15:44:21 +0900

    feat: update PostIndex

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,25 +1,22 @@
 import {createStore} from "redux"
 
-const selectOpt = text => {
-  return {
-    type: "option",
-    text
+// useEffect(async ()=>{
+//   //api : http://localhost:3000/v1/post
+//     const json = await axios.get("http://localhost:3000/data/postData.json")
+//     setposts(json.data)
+//   }, []
+// )
+
+const reducer = (state = [], action) => {
+  
+  if (action.type === "recent"){
+    return [{text:'POPULAR'}]
+  } else {
+    return [{text:'RECENT'}]
   }
 }
 
-const reducer = (state = ['hello'], action) => {
-  console.log(action)
-  switch (action.type) {
-    case state:
-      return ['hello']
-    case state:
-      return ['hello']
-  }
-}
 
-export const actionCreators = {
-  selectOpt,
-}
 
 const store = createStore(reducer)
 export default store;

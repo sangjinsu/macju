9766df7dd306292ac765ef40a6d241d5570ae65c
jinsuSang <climbplant39@kakao.com>
2022-01-21 11:36:55 +0900

    b/feat: create beer beertype memberRateBeer jpa entity

---
+++ backend/MacJU/src/main/java/com/sib/macju/domain/Beer.java
@@ -0,0 +1,38 @@
+package com.sib.macju.domain;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "beer")
+public class Beer {
+
+    @Id
+    @Column(name = "beer_id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long beerId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "beer_type_id")
+    private BeerType beerType;
+
+    @Column(unique = true)
+    private String name;
+
+    private String content;
+
+    private Double volumn;
+
+    private String englishName;
+
+    @OneToMany(mappedBy = "beer", cascade = CascadeType.ALL)
+    private List<MemberRateBeer> memberRateBeers = new ArrayList<>();
+
+}
---
+++ backend/MacJU/src/main/java/com/sib/macju/domain/BeerType.java
@@ -0,0 +1,26 @@
+package com.sib.macju.domain;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "beer_type")
+public class BeerType {
+
+    @Id
+    @Column(name = "beer_type_id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long beerTypeId;
+
+    private String detail;
+
+    @OneToMany(mappedBy = "beerType", cascade = CascadeType.ALL)
+    private List<Beer> beers = new ArrayList<>();
+}

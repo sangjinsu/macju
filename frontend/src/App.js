2b63920d429ec472759490380f9b41a1f4ac8037
K-dongil <kdi1569@gmail.com>
2022-01-21 20:22:16 +0900

    f/feat: create signup, nicknameCheck

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import PostDetail from "./routes/Post/PostDetail";
 import PostCreate from "./routes/Post/PostCreate";
 import CommentList from "./routes/Post/CommentList";
 import BeerList from './routes/Beer/BeerList.js';
+import Signup from './routes/Signup';
 
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
         <Route path="/post/new"><PostCreate /></Route>
         <Route path="/post/:num"><PostDetail /><CommentList /></Route>
         <Route path='/beer'><BeerList /></Route>
+        <Route path='/signup'><Signup /></Route>
       </Switch>
     </Router>
   );

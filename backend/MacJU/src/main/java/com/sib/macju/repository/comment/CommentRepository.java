22feff228f836785621b982806abce9c0b729ad1
suhan <suhan9603@naver.com>
2022-01-26 01:41:25 +0900

    b/feat: create beartype post comment spring data jpa

--- backend/MacJU/src/main/java/com/sib/macju/repository/comment/CommentRepository.java
+++ backend/MacJU/src/main/java/com/sib/macju/repository/comment/CommentRepository.java
@@ -1,8 +1,11 @@
 package com.sib.macju.repository.comment;
 
+import com.sib.macju.domain.comment.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public interface CommentRepository {
+// 댓글 생성, 다수 조회, 삭제 // 단일 조회, 수정은 X
 
+@Repository
+public interface CommentRepository extends JpaRepository<Comment, Long> {
 }
--- backend/MacJU/src/main/java/com/sib/macju/service/comment/CommentService.java
+++ backend/MacJU/src/main/java/com/sib/macju/service/comment/CommentService.java
@@ -2,6 +2,15 @@ package com.sib.macju.service.comment;
 
 import org.springframework.stereotype.Service;
 
+
+
+
+//searchBestPosts()
+//deleteComment()
+
+//쿼리
+//fetchComments()
+
 @Service
 public class CommentService {
 

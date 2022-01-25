696ad19533190321bccfa289da1b5c30cf824b5e
jinsuSang <climbplant39@kakao.com>
2022-01-25 11:10:31 +0900

    b/feat: create member Like beer table

--- backend/MacJU/src/main/java/com/sib/macju/domain/MemberLikePost.java
+++ backend/MacJU/src/main/java/com/sib/macju/domain/MemberLikePost.java
@@ -1,9 +1,22 @@
 package com.sib.macju.domain;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "member_like_post")
 public class MemberLikePost {
+    @Id
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 }

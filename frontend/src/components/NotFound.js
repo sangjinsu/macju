6705ef6c07b11234ee993b86d4635a1d7c975343
K-dongil <kdi1569@gmail.com>
2022-01-26 13:40:06 +0900

    f/feat: create-404page

---
+++ frontend/src/components/NotFound.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './NotFound.css'
+import { ReactComponent as Arrow } from './logo.svg';
+ 
+function RecommendBeer(){
+  return(
+    <div class="contain" id="animate">
+      <p>test</p>
+      <Arrow ></Arrow>
+    </div>
+  )
+}
+
+export default RecommendBeer;

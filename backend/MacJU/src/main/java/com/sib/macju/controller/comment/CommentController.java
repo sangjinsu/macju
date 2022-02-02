6affe6cd96d9f65c7a1de67cb9106d0dfd2cd8fb
wajabu <suhan9603@naver.com>
2022-02-03 05:08:07 +0900

    b/feat:create comment dto update comment

--- backend/MacJU/src/main/java/com/sib/macju/controller/comment/CommentController.java
+++ backend/MacJU/src/main/java/com/sib/macju/controller/comment/CommentController.java
@@ -3,7 +3,9 @@ package com.sib.macju.controller.comment;
 import com.sib.macju.domain.comment.Comment;
 import com.sib.macju.domain.member.Member;
 import com.sib.macju.domain.post.Post;
+import com.sib.macju.dto.comment.RequestCreateCommentDto;
 import com.sib.macju.repository.comment.CommentRepository;
+import com.sib.macju.repository.member.MemberRepository;
 import com.sib.macju.repository.post.PostRepository;
 import com.sib.macju.service.comment.CommentService;
 import lombok.NonNull;
@@ -27,57 +29,57 @@ public class CommentController {
     private final PostRepository postRepository;
     @NonNull
     private final CommentService commentService;
+    @NonNull
+    private final MemberRepository memberRepository;
 
-    // 로그인 되어 있는 멤버 추출
-    private final HttpSession httpSession;
 
-    // create
     @PostMapping("/{postId}/comment")
-    public Comment createComment(@PathVariable Long postId, @RequestBody Comment comment){
-        Post post = postRepository.findById(postId).orElse(null);
-        // memberId = ((SessionUser) httpSession.getAttribute("member")).getId();
-        // https://github.com/hahyuning/SpringBoot-Project/blob/60ccff3dadc279f2626dcc5dc89b813df9c994a6/src/main/java/com/desk/spring/config/oauth/dto/SessionUser.java
+    public Long createComment(@PathVariable Long postId, @RequestBody RequestCreateCommentDto requestCreateCommentDto){
 
-        Long memberId = Long.valueOf(1);
-        Comment result = commentService.createComment(post.getPostId(), comment, memberId);
-
-        // @AuthenticationPrincipal // 인증된 사용자 member 반환
-        //Comment result = commentService.createComment(post.getPostId(), comment,  @AuthenticationPrincipal UserDetailsImpl userDetails);
-        // https://github.com/seongbinko/remember_dream_v2/blob/main/src/main/java/shop/dream/config/UserDetailsImpl.java
-        // https://github.com/seongbinko/remember_dream_v2/blob/main/src/main/java/shop/dream/controller/CommentController.java
-
-        return result;
+        return commentService.createComment(
+                postId,
+                requestCreateCommentDto.getMemberId(),
+                requestCreateCommentDto.getContent()
+        );
     }
 
-//    public ResponseEntity createComment (@PathVariable Long postId, @RequestBody Comment comment) {
-//        Post post = postRepository.findById(postId).orElse(null);
-//        if(post == null) {
-//            return ResponseEntity.badRequest().build();
-//        }
-//        commentService.createComment(post.getPostId(), comment);
-//        return ResponseEntity.ok().build();
-//    }
-
-    // get
     @GetMapping("/{postId}/comment")
     public List<Comment> fetchComments(@PathVariable Long postId) {
         return commentService.fetchComments(postId);
     }
 
-    // delete
     @DeleteMapping("/{postId}/comment/{commentId}")
-    public List<Comment> deleteComment(@PathVariable Long postId, @PathVariable Long commentId){ // (Authentication authentication)        Optional<Comment> comment  = commentRepository.findById(commentId);
-        return commentService.deleteComment(postId, commentId);
-
-//        String username = authentication.getName();
-//        if (username == comment.get().getMember().getNickName()) {
-//            return commentService.deleteComment(postId, commentId);
-//        }
-//        return null;
+    public void deleteComment(@PathVariable Long postId, @PathVariable Long commentId){ // (Authentication authentication)        Optional<Comment> comment  = commentRepository.findById(commentId);
+       commentService.deleteComment(commentId);
     }
-
-    // 사용자 인증 추가??
-//    public List<Comment> deleteComment(@PathVariable Long postId, @PathVariable Long commentId, @AuthenticationPrincipal UserDetailsImpl userDetails){
-//        return commentService.deleteComment(postId, commentId, userDetails.getAccount().getId());
-
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

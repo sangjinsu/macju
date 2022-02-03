79f98750d7892f3aa8d5e40f6ace870ee9301424
K-dongil <kdi1569@gmail.com>
2022-02-03 13:49:42 +0900

    f/update: profile

--- frontend/src/routes/User/Profile.js
+++ frontend/src/routes/User/Profile.js
@@ -1,5 +1,4 @@
 import UserProfile from "../../components/User/UserProfile.js"
-
 import UserPost from "../../components/User/UserPost.js";
 import UserLike from "../../components/User/UserLike.js";
 import UserReview from "../../components/User/UserReview.js";
@@ -10,8 +9,20 @@ import { Link } from "react-router-dom"
 const Profile = () => {
   return (
     <div>
-      
+      <UserProfile />
+      <Link to="/profile/post">post</Link> <br />
+      <Link to="/profile/like">like</Link> <br />
+      <Link to="/profile/review">review</Link> <br />
+      <Route path="/profile/post">
+        <UserPost />
+      </Route>
+      <Route path="/profile/like">
+        <UserLike />
+      </Route>
+      <Route path="/profile/review">
+        <UserReview />
+      </Route>
     </div>
-  )
+  );
 }
 export default Profile;

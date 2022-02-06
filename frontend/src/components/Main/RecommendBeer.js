1900f0002b109e26f6aaa17cb52aed5ac15e228c
K-dongil <kdi1569@gmail.com>
2022-02-06 19:37:32 +0900

    f/feat: create-RecommendBeerCarousel

--- frontend/src/components/Main/RecommendBeer.js
+++ frontend/src/components/Main/RecommendBeer.js
@@ -1,91 +1,76 @@
-import React from 'react';
-// import './RecommendBeer.css'
- 
- 
-function RecommendBeer(){
+import React, { useEffect } from 'react';
+import Slider from "react-slick";
+import "./RecommendBeer.css"
+
+
+const RecommendBeer = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    autoplay: true,
+    // className: "center",
+    // centerMode: true,
+    // centerPadding: "60px",
+    fade:true, //center랑 중복 불가능
+    autoplaySpeed: 1000,
+    speed: 1000,
+    slidesToShow: 1,
+    slidesToScroll: 1
+  };
+
+  useEffect( () => {
+    A()
+  }, [])
+
   return(
-    <div className="hero_area">
-      <div className="bg-box">
-        <img src="img/hero-bg.jpg" alt="" />
-      </div>
-      <section className="slider_section ">
-        <div id="customCarousel1" className="carousel slide" data-ride="carousel">
-          <div className="carousel-inner">
-            <div className="carousel-item active">
-              <div className="container ">
-                <div className="row">
-                  <div className="col-md-7 col-lg-6 ">
-                    <div className="detail-box">
-                      <h1>
-                        Fast Food Restaurant
-                      </h1>
-                      <p>
-                        Doloremque, itaque aperiam facilis rerum, commodi, temporibus sapiente ad mollitia laborum quam quisquam esse error unde. Tempora ex doloremque, labore, sunt repellat dolore, iste magni quos nihil ducimus libero ipsam.
-                      </p>
-                      <div className="btn-box">
-                        <a  className="btn1">
-                          Order Now
-                        </a>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="carousel-item ">
-              <div className="container ">
-                <div className="row">
-                  <div className="col-md-7 col-lg-6 ">
-                    <div className="detail-box">
-                      <h1>
-                        Fast Food Restaurant
-                      </h1>
-                      <p>
-                        Doloremque, itaque aperiam facilis rerum, commodi, temporibus sapiente ad mollitia laborum quam quisquam esse error unde. Tempora ex doloremque, labore, sunt repellat dolore, iste magni quos nihil ducimus libero ipsam.
-                      </p>
-                      <div className="btn-box">
-                        <a className="btn1">
-                          Order Now
-                        </a>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="carousel-item">
-              <div className="container ">
-                <div className="row">
-                  <div className="col-md-7 col-lg-6 ">
-                    <div className="detail-box">
-                      <h1>
-                        Fast Food Restaurant
-                      </h1>
-                      <p>
-                        Doloremque, itaque aperiam facilis rerum, commodi, temporibus sapiente ad mollitia laborum quam quisquam esse error unde. Tempora ex doloremque, labore, sunt repellat dolore, iste magni quos nihil ducimus libero ipsam.
-                      </p>
-                      <div className="btn-box">
-                        <a  className="btn1">
-                          Order Now
-                        </a>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="container">
-            <ol className="carousel-indicators">
-              <li data-target="#customCarousel1" data-slide-to="0" className="active"></li>
-              <li data-target="#customCarousel1" data-slide-to="1"></li>
-              <li data-target="#customCarousel1" data-slide-to="2"></li>
-            </ol>
-          </div>
+    <div className="SlickTest">
+        <div>
+            <h3>Best Beer</h3>
         </div>
-      </section>
+        <div id="bubbles">
+          <Slider {...settings}>
+              <CustomSlide index={1}></CustomSlide>
+              <CustomSlide index={2}></CustomSlide>
+              <CustomSlide index={3}></CustomSlide>
+              <CustomSlide index={4}></CustomSlide>
+          </Slider>
+        </div>
     </div>
   )
 }
+
+function CustomSlide(props) {
+  const imgSrc = "img/abc.png"
+  return(
+    <div {...props}>
+      <img src={imgSrc}/>
+    </div>
+  )
+}
+
+function A(){
+  let bubbleEnd
+  const bubbles = document.getElementById('bubbles'),
+  
+  randomN = function(start, end){
+    return Math.random()*end+start;
+  }
+  let i = 0
+  let generateBubble = function(){
+    if(i < 25){
+      const bubble = document.createElement('div'),
+          size = randomN(5, 10);
+          bubble.setAttribute('style','width: '+size+'px; height: '+size+'px; left:'+randomN(size, bubbles.offsetWidth-(size+5) )+'px;'); // 방울크기, 방울을 어느 위치에서 띄울것인지
+          bubble.setAttribute("class", "bubble")
+      bubbles.appendChild(bubble);
+      i++;
+    }else{
+      clearInterval(bubbleEnd); // 충분한 방울 생성되면, setInterval 반환값을 인자로 받아 setInterval 종료
+    }
+  };
+
+  bubbleEnd = setInterval(generateBubble, 500); // generateBubble를 시간을 두고 실행 
+}
+
 
 export default RecommendBeer;

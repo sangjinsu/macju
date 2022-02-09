bd755edad8c6ef1aa0cb39dd3827121776057953
ssong_14 <ssong_14@naver.com>
2022-02-09 17:03:56 +0900

    env update

--- frontend/src/routes/Beer/BeerDetail.js
+++ frontend/src/routes/Beer/BeerDetail.js
@@ -15,6 +15,10 @@ function BeerDetail() {
   const BEER_DETAIL_POST_URL = process.env.REACT_APP_SERVER + ':8080/v1/post/beer'
   const BEER_DETAIL_LOG_URL = process.env.REACT_APP_SERVER + ':8080/v1/log'
   const RANKING_BEER_URL = process.env.REACT_APP_SERVER + ':8081/beer/view'
+
+
+
+
   // 맥주 data
   const [beer, setbeer] = useState()
   // 맥주의 posts

e66155e8a0c4feb5c9b8d5d922915041dbeff640
K-dongil <kdi1569@gmail.com>
2022-02-11 12:37:13 +0900

    f/refactore: useCallback, solve errorMessage

--- frontend/src/routes/User/Signup.js
+++ frontend/src/routes/User/Signup.js
@@ -1,80 +1,98 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
+import { useCallback } from 'react';
 import { useDispatch } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 import '../../styles/Signup.css'
 
 function Signup(props) {
   const VALIDATE_NICKNAME_URL = process.env.REACT_APP_SERVER + ':8080/v1/member/validatenickname'
   const USER_SIGNUP_URL = process.env.REACT_APP_SERVER + ':8080/v1/member/signup'
   const userData = props.location.userData
-  const dispatch = useDispatch()
   
+  const dispatch = useDispatch()
+  const history = useHistory()
   const [nickname, nicknameChange] = useState("");
   const [age, ageChange] = useState("");
   const [gender, setGender] = useState("");
   const [AvailableNick, setAvailable] = useState()
   const [submitBtn, deactivateSubmitBtn] = useState(true)
   
+  // nickname 변경
   const changeNickName = e => {
     nicknameChange(e.target.value);
   }
 
+  // gender 변경
   const selectValue = e => {
-    if (e.target.value) {
-      setGender(e.target.value); 
-    }
+    setGender(e.target.value); 
   }
 
+  // 나이 변경
   const changeAge = e => {
     ageChange(e.target.value);
   }
 
-  const signupBtn = e => {
-    e.preventDefault()
-    const signup = async() => {
-      try{
-        const singupData = { // 액세스 토큰 나중에 넣어줘야 함
-          // "memberId": userData.member.kakaoMemberId,
-          "age": age,
-          "name": userData.member.nickName,
-          "nickName": nickname
-        }
-        const headers = {
-          headers: {
-            "Accept" : "application/json;charset=UTF-8",
-            "Content-Type" : "application/json;charset=UTF-8"
-          }
-        }
-        await axios.post(USER_SIGNUP_URL, singupData, headers)
 
-      }catch{
-        console.log("회원가입 실패")
+  // 회원가입 버튼 누를시 동작
+  const signupBtn = useCallback( async () => {
+    try{
+      if (age < 20) {
+        alert("미성년자는 가입 할 수 없습니다")
+        history.replace("/home")
+      } 
+
+      const singupData = { // 액세스 토큰 나중에 넣어줘야 함
+        // "memberId": userData.member.kakaoMemberId,
+        "age": age,
+        "name": userData.member.nickName,
+        "nickName": nickname
       }
-    }
-    signup()
-    dispatch({type:"loginSucess", userData:userData})
-  }
+      const headers = {
+        headers: {
+          "Accept" : "application/json;charset=UTF-8",
+          "Content-Type" : "application/json;charset=UTF-8"
+        }
+      }
+      const test = await axios.post(USER_SIGNUP_URL, singupData, headers)
+      console.log(test)
 
-  useEffect( () => { // 닉네임 수정할 때 버튼 막아놓고 시작
+    }catch{
+      // 회원가입 실패시 알람 + 로그인 페이지 다시 이동
+      alert("회원가입 실패")
+      history.replace("/login")
+    }
+    dispatch({type:"loginSucess", userData:userData})
+    history.push("/home")
+  }, [age, userData, nickname, USER_SIGNUP_URL, dispatch, history])
+
+  // 닉네임 수정할 때 버튼 막아놓고 시작
+  useEffect( () => { 
     deactivateSubmitBtn(true)
     }, [nickname]
   )
 
-  useEffect( () => {
-    const validationNinkname = async () => {
-      try{
-        const { data : nicknameStatus } = await axios.get(`${VALIDATE_NICKNAME_URL}/${nickname}`)
-        if (nicknameStatus === "success") {
-          setAvailable(true)
-        }else {
-          setAvailable(false)
-        }
-      }catch{
-        console.log("중복확인 실패")
+  const validationNinkname = useCallback( async () => {
+    try{
+      const { data : nicknameStatus } = await axios.get(`${VALIDATE_NICKNAME_URL}/${nickname}`)
+      console.log(nicknameStatus)
+      if (nicknameStatus === "success") {
+        setAvailable(true)
+      }else {
+        setAvailable(false)
       }
+    }catch{
+      console.log("중복확인 실패")
     }
-    validationNinkname()
-    }, [nickname]
+  }, [nickname, VALIDATE_NICKNAME_URL])
+
+  useEffect( () => {
+    if (nickname) {
+      validationNinkname()
+    }else {
+      setAvailable(false)
+    }
+    }, [nickname, validationNinkname]
   )
 
   useEffect( () => { // 수정필요
@@ -112,7 +130,6 @@ function Signup(props) {
                       onChange={ changeNickName }
                     />
                   </div>
-                  {/* <button type="button" onClick={ checkNickName } disabled={ nickBtn }>중복확인</button> <br/> */}
                   
                   {/* 나이 입력 */}
                   <div>

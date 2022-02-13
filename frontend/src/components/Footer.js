feb4d93ca3222645572349bbe13ddf79d3d43bd8
K-dongil <kdi1569@gmail.com>
2022-02-14 00:04:51 +0900

    f/style: footer content setting

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -11,75 +11,46 @@ function Footer(){
         
         <div className="col-md-4 footer-col">
           <div className="footer_detail">
-            <a href="https://edu.ssafy.com/comm/login/SecurityLoginForm.do" className="footer-logo">
-              MacJU
-            </a>
-            <p>
-              SSAFY 1st Project
-            </p>
-            <p>상진수, 장정훈, 최수한 </p>
-            <p>김현송, 김동일, 김소희 </p>
-            {/* <div className="footer_social">
-              <a href="https://edu.ssafy.com/comm/login/SecurityLoginForm.do">
-                <i className="fa fa-facebook" aria-hidden="true"></i>
-              </a>
-              <a href="https://edu.ssafy.com/comm/login/SecurityLoginForm.do">
-                <i className="fa fa-twitter" aria-hidden="true"></i>
-              </a>
-              <a href="https://edu.ssafy.com/comm/login/SecurityLoginForm.do">
-                <i className="fa fa-linkedin" aria-hidden="true"></i>
-              </a>
-              <a href="https://edu.ssafy.com/comm/login/SecurityLoginForm.do">
-                <i className="fa fa-instagram" aria-hidden="true"></i>
-              </a>
-              <a href="https://edu.ssafy.com/comm/login/SecurityLoginForm.do">
-                <i className="fa fa-pinterest" aria-hidden="true"></i>
-              </a>
-            </div> */}
+            <h2 className="footer-logo">
+              Our Service
+            </h2>
+            <p>Community</p>
+            <p>Recommend Beer</p>
+            <p>Beer Info</p>
           </div>
         </div>
 
 
         <div className="col-md-4 footer-col">
           <div className="footer_contact">
-            <h4>
+            <h4 className="footer-logo">
               Contact Us
             </h4>
             <div className="contact_link_box">
-              <a href="https://edu.ssafy.com/comm/login/SecurityLoginForm.do">
-                <i className="fa fa-map-marker" aria-hidden="true"></i>
-                <span>
-                  Location
-                </span>
-              </a>
-              <a href="https://edu.ssafy.com/comm/login/SecurityLoginForm.do">
-                <i className="fa fa-phone" aria-hidden="true"></i>
-                <span>
-                  Call +01 1234567890
-                </span>
-              </a>
-              <a href="https://edu.ssafy.com/comm/login/SecurityLoginForm.do">
+              <a href="mailto:macju.service@gmail.com">
                 <i className="fa fa-envelope" aria-hidden="true"></i>
                 <span>
-                  sopee9814@gmail.com
+                  MacJu.service@gmail.com
                 </span>
               </a>
+              <span>
+                오전9시 ~ 오후 6시 (주말 및 공휴일 휴무)
+              </span>
             </div>
           </div>
         </div>
 
 
-        {/* <div className="col-md-4 footer-col">
-          <h4>
-            Opening Hours
-          </h4>
+        <div className="col-md-4 footer-col">
+          <h2 className="footer-logo">
+            MacJU
+          </h2>
           <p>
-            Everyday
+            SSAFY 1st Project
           </p>
-          <p>
-            00.00 Am -12.00 Pm
-          </p>
-        </div> */}
+          <p>상진수, 장정훈, 최수한 </p>
+          <p>김현송, 김동일, 김소희 </p>
+        </div>
       </div>
       <div className="footer-info">
         <p>

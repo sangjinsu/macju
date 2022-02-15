7770de2a175c2a487e84f8d78e76f93d14b7899f
K-dongil <kdi1569@gmail.com>
2022-02-15 22:02:06 +0900

    f/feat: custom Axios

--- frontend/src/CustomAxios.js
+++ frontend/src/CustomAxios.js
@@ -1,9 +1,10 @@
 import axios from "axios";
 
-
 const axiosInstance = axios.create({
   headers:{
-    AccessToken:  window.localStorage.getItem("AccessToken")
+    AccessToken:window.localStorage.getItem("AccessToken"),
+    "Accept":"application/json;charset=UTF-8",
+    "Content-Type":"application/json;charset=UTF-8"
   }
 })
 

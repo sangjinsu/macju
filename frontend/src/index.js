d729b776ce297d4c45c7af4033f5b1878496d691
K-dongil <kdi1569@gmail.com>
2022-02-17 17:09:53 +0900

    f/feat: index.js Cookie setting

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,18 +7,18 @@ import persistedReducer from "./store"
 import { PersistGate } from 'redux-persist/integration/react';
 import { createStore } from 'redux';
 import {persistStore} from "redux-persist"
+import { CookiesProvider } from 'react-cookie';
 
 const store = createStore(persistedReducer);
 const persistor = persistStore(store);
-
-
-
 
 ReactDOM.render(
   
     <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
+    <CookiesProvider>
       <App />
+    </CookiesProvider>
     </PersistGate>
     </Provider>
   ,

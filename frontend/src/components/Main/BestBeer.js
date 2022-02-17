67d09b34edb74c378d0e7dd4a3aec435d5cb2d92
K-dongil <kdi1569@gmail.com>
2022-02-18 05:39:25 +0900

    f/css: css update

--- frontend/src/components/Main/BestBeer.js
+++ frontend/src/components/Main/BestBeer.js
@@ -3,7 +3,6 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import axiosInstance from "CustomAxios";
-import { Link } from "react-router-dom";
 const BestBeer = (props) => {
   const [rankingBeerList, setRanking] = useState()
 
@@ -13,36 +12,36 @@ const BestBeer = (props) => {
   settings.slidesToShow = 1
 
   const PopBeer = async () => {
-    try{
+    try {
       const RANKING_POPBEER = process.env.REACT_APP_SERVER + ':8888/beer/popbeer'
-      const { data : rankingBeer } = await axiosInstance.get(RANKING_POPBEER)
-      const rankingBeerId = rankingBeer.map( (beer) => beer.beerId )
+      const { data: rankingBeer } = await axiosInstance.get(RANKING_POPBEER)
+      const rankingBeerId = rankingBeer.map((beer) => beer.beerId)
       setRanking(rankingBeerId)
-    }catch{
+    } catch {
       setRanking(false)
     }
   }
-  useEffect( () => {
+  useEffect(() => {
     PopBeer()
   }, [])
 
-  return(
+  return (
     <div className="SlickTest">
       <h3 className="bestbeer pt-4" align="center">Best Beer</h3>
       {
         rankingBeerList
-        ?
-        <Slider {...settings}>
-          {
-            rankingBeerList&&rankingBeerList.map((beerid, i) => 
-              <CustomSlide beerid={beerid} key={i} />
-            )
-          }
-        </Slider>
-        :
-        <div className="main_none"> 
-          <div>맥주에게 관심을 주세요!!</div>
-        </div>
+          ?
+          <Slider {...settings}>
+            {
+              rankingBeerList && rankingBeerList.map((beerid, i) =>
+                <CustomSlide beerid={beerid} key={i} />
+              )
+            }
+          </Slider>
+          :
+          <div className="main_none">
+            <div>맥주에게 관심을 주세요!!</div>
+          </div>
       }
     </div>
   )
@@ -55,24 +54,21 @@ function CustomSlide(props) {
   const [imgSrc, setImgSrc] = useState()
   const [beerName, setName] = useState()
   const [urlBeerId, setBeerId] = useState()
-  useEffect( () => {
-    const fetchData = async ()=>{
-      const { data : beerDetail } = await axiosInstance.get(`${BEER_DETAIL_URL}/${props.beerid}`)
+  useEffect(() => {
+    const fetchData = async () => {
+      const { data: beerDetail } = await axiosInstance.get(`${BEER_DETAIL_URL}/${props.beerid}`)
       setImgSrc(beerDetail.photoPath)
       setName(beerDetail.name)
       setBeerId(beerDetail.beerId)
     }
     fetchData();
   }, [BEER_DETAIL_URL, props.beerid])
-  return(
+  return (
     <div {...props} className="best_beers row text-center">
-      <Link to={`/beer/${urlBeerId}`}>
-        <img className="slideImg best_beer_img col w-50 " src={imgSrc} alt="" style={{maxHeight:400, maxWidth:300 }}/>
-        <div className="slideDiv best_beer_content col text-center mb-5">
-          <h3 className="beer_name">{beerName}</h3>
-          <div></div>
-        </div>
-      </Link>
+      <img className="slideImg best_beer_img col w-50 " src={imgSrc} alt="" style={{ maxHeight: 400, maxWidth: 300 }} />
+      <div className="slideDiv best_beer_content col text-center mb-5">
+        <h3 className="beer_name">{beerName}</h3>
+      </div>
     </div>
   )
 }

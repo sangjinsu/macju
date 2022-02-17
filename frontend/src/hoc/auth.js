7da2552b3c644da5094068811407f70f46a516d5
K-dongil <kdi1569@gmail.com>
2022-02-17 17:10:34 +0900

    f/refactor: auth (localStroage->cookie)

--- frontend/src/hoc/auth.js
+++ frontend/src/hoc/auth.js
@@ -1,19 +1,19 @@
 import React from "react"
 import { useEffect } from "react";
-import { useSelector } from "react-redux";
-
+import {useCookies} from "react-cookie";
 
 const auth = (Component, option) => {
   const AuthenticateCheck = (props) => {
-    const isUser = useSelector(state => state.userReducer)
-    const memberId = isUser.memberId
+    const cookies = useCookies(["AccessToken"])[0];
+    const AccessToken = cookies.AccessToken
+    
     useEffect(() => {
-      if (!memberId && option) { 
+      if (!AccessToken && option) { // 로그인이 필요한 page에, 로그인하지 않은 유저가 접근하려고 할 때
         props.history.push("/user/login");
-      } else if (memberId && !option) { 
+      } else if (AccessToken && !option) { // 로그인이 필요하지 않은 page에, 로그인한 유저가 접근하려고 할 때
         props.history.push("/home")
       }
-    }, [memberId, props.history]);
+    }, [AccessToken, props.history]);
     return (
       <Component {...props} />
     )

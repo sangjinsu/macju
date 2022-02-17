6a51da191ff67adc018a51a6caad75aac18362ed
K-dongil <kdi1569@gmail.com>
2022-02-18 04:37:03 +0900

    f/refactor: code update

--- frontend/src/CustomAxios.js
+++ frontend/src/CustomAxios.js
@@ -1,17 +1,9 @@
 import axios from "axios";
-
-const cookieList = document.cookie.split(";")
-let accessCookie = ""
-for (let i = 0; i < cookieList.length; i++) {
-  const nowCookie = cookieList[i].split("=")[0]
-  if (nowCookie === "AccessToken") {
-    accessCookie = cookieList[i].split("=")[1]
-  }
-}
+import AccessToken from "./AccessToken.js"
 
 const axiosInstance = axios.create({
   headers: {
-    AccessToken: accessCookie,
+    AccessToken: AccessToken,
     "Accept": "application/json;charset=UTF-8",
     "Content-Type": "application/json;charset=UTF-8"
   }

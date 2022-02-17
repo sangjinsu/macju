f06378679bc1ce64caba9442e5c6f138be872cd4
K-dongil <kdi1569@gmail.com>
2022-02-18 03:57:15 +0900

    f/fix: cookie update

--- frontend/src/CustomAxios.js
+++ frontend/src/CustomAxios.js
@@ -1,10 +1,19 @@
 import axios from "axios";
 
+const cookieList = document.cookie.split(";")
+let accessCookie = ""
+for (let i = 0; i < cookieList.length; i++) {
+  const nowCookie = cookieList[i].split("=")[0]
+  if (nowCookie === "AccessToken") {
+    accessCookie = cookieList[i].split("=")[1]
+  }
+}
+
 const axiosInstance = axios.create({
-  headers:{
-    AccessToken:document.cookie.split("=")[1],
-    "Accept":"application/json;charset=UTF-8",
-    "Content-Type":"application/json;charset=UTF-8"
+  headers: {
+    AccessToken: accessCookie,
+    "Accept": "application/json;charset=UTF-8",
+    "Content-Type": "application/json;charset=UTF-8"
   }
 })
 

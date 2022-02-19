05023db492e88483070363d16a6e59be11caa535
ssong_14 <ssong_14@naver.com>
2022-02-19 13:22:26 +0900

    f/pull git

--- frontend/src/components/Main/RecommendBeer.js
+++ frontend/src/components/Main/RecommendBeer.js
@@ -3,7 +3,6 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import axios from "axios";
-import { useState } from "react";
 import { useCallback } from "react";
 
 
